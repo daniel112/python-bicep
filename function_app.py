16d8cc16b1db9c05a1f2ba4e106ddb9a37b98864
danielyo <danielyo112@gmail.com>
2024-06-10 17:43:16 -0700

    managed id login

--- function_app.py
+++ function_app.py
@@ -15,13 +15,15 @@ def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
         then attempts to deploy the bundled bicep file to the Azure subscription tied to the DefaultAzureCredential.
     '''
     logging.info('---Authenticating with Default Creds---')
-    # account_url = "https://dyobicepstorage2.blob.core.windows.net"
-    _ = DefaultAzureCredential()
+    # credential = DefaultAzureCredential()
+    # token = credential.get_token('https://management.azure.com/.default')
+    # logging.info('---Token---')
+    # logging.info(token.token)
+    run_az_cli_command(['login', '--identity'])
+
     logging.info('---Authenticated---')
-    # run_az_cli_command(['group', 'list'])
     run_az_cli_command(["group", "list"])
-    # Create the BlobServiceClient object
-    # blob_service_client = BlobServiceClient(account_url, credential=credentials)
+
     current_dir = os.path.dirname(__file__)
     file_path = os.path.join(current_dir, 'bicep', 'cosmos.bicep')
     logging.info('---BICEP FILE PATH---')
@@ -31,10 +33,10 @@ def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
     if os.path.exists(file_path):
         logging.info('File exists, do the thing')
         # az deployment group create --resource-group DefaultResourceGroup-WUS --template-file cosmos.bicep
-        command = ["az", "deployment", "group", "create", 
+        command = ["deployment", "group", "create", 
                    "--resource-group", "DefaultResourceGroup-WUS", 
                    "--template-file", file_path]
-        # run_az_cli_command(command)
+        run_az_cli_command(command)
 
     else:
         logging.info('File DNE')
